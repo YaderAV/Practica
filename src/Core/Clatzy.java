dc0f6c4e933641a4add33830a677b1c6bb261f77
Yader <yaderv@uninorte.edu.co>
2025-04-06 01:45:00 -0500

    verificacion

--- src/Core/Clatzy.java
+++ src/Core/Clatzy.java
@@ -62,6 +62,6 @@ public class Clatzy {
     }
 
     public Plan getPlan(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       
     }
 }
