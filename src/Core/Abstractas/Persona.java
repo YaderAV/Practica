13360c064efeeee65bc12bb3b7cac17eb7ccef78
Yader <yaderv@uninorte.edu.co>
2025-04-06 01:07:15 -0500

    Creacion clase persona

--- src/Core/Abstractas/Persona.java
+++ src/Core/Abstractas/Persona.java
@@ -9,5 +9,17 @@ package Core.Abstractas;
  * @author yader
  */
 public class Persona {
+ 
+    protected String nombre;
+    protected String cedula;
+    protected String telefono;
+    protected String email;
+
+    public Persona(String nombre, String cedula, String telefono, String email) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.telefono = telefono;
+        this.email = email;
+    }
     
 }
